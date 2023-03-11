1fadb00d81149bb4cb0128e08e71820b34520896
Mikołaj Bartnicki <mbrtnck@gmail.com>
2023-03-11 23:22:42 +0100

    Add task solutions

---
+++ FixTheAddition/script.js
@@ -0,0 +1,8 @@
+// It works incorrectly. The output is 12. Fix it. The result should be 3.
+
+"use strict";
+
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(+a + +b);
