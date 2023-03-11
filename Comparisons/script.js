46bf0baad096aaaceff946ccad5bcf69e21c50ea
Mikołaj Bartnicki <mbrtnck@gmail.com>
2023-03-11 23:37:48 +0100

    Add task solution

---
+++ Comparisons/script.js
@@ -0,0 +1,11 @@
+// What will be the result of these expressions?
+
+"use strict";
+
+5 > 4                   // true
+"apple" > "pinneapple"  // false
+"2" > "12"              // true
+undefined == null       // true
+undefined === null      // false
+null == "\n0\n"         // false
+null === +"\n0\n"       // false
